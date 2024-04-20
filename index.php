0005b2b1bdc1206e27d3ac11a3fe3ded58d08a3a
Ali Mousavi <38689441+alimousavi68@users.noreply.github.com>
2024-04-20 15:10:47 +0330

    for test

--- index.php
+++ index.php
@@ -7,189 +7,189 @@ Author: Your Name
 */
 
 
-exit;
+// exit;
 
 
-// Hook into WordPress actions
-add_action('admin_init', 'custom_rss_parser_schedule_event');
+// // Hook into WordPress actions
+// add_action('admin_init', 'custom_rss_parser_schedule_event');
 
-// Add action to create custom table if not exists
-add_action('admin_init', 'custom_rss_parser_create_table');
+// // Add action to create custom table if not exists
+// add_action('admin_init', 'custom_rss_parser_create_table');
 
-// Schedule event to run every 5 minutes
-function custom_rss_parser_schedule_event()
-{
-    // Schedule the event to run every 5 minutes
-    if (!wp_next_scheduled('custom_rss_parser_event')) {
-        wp_schedule_event(time(), '5minutes', 'custom_rss_parser_event');
-    }
+// // Schedule event to run every 5 minutes
+// function custom_rss_parser_schedule_event()
+// {
+//     // Schedule the event to run every 5 minutes
+//     if (!wp_next_scheduled('custom_rss_parser_event')) {
+//         wp_schedule_event(time(), '5minutes', 'custom_rss_parser_event');
+//     }
 
-    if (!wp_next_scheduled('remove_all_feed_on_feeds_table')) {
-        wp_schedule_event(time(), 'i8_daily_cron', 'remove_all_feed_on_feeds_table');
-    }
-}
+//     if (!wp_next_scheduled('remove_all_feed_on_feeds_table')) {
+//         wp_schedule_event(time(), 'i8_daily_cron', 'remove_all_feed_on_feeds_table');
+//     }
+// }
 
-add_filter('cron_schedules', 'i8_register_daily_cron_schedule');
-function i8_register_daily_cron_schedule($schedules)
-{
-    $schedules['i8_daily_cron'] = array(
-        'interval' => (60 * 60) * 24,
-        'display' => __('این کرون هر ۲۴ ساعت اجرا میشود')
-    );
-    $schedules['5minutes'] = array(
-        'interval' => (5 * 60),
-        'display' => __('این کرون هر ۵دقیقه اجرا میشود')
-    );
-    return $schedules;
-}
-add_action('remove_all_feed_on_feeds_table', 'remove_all_feed_on_feeds_table');
+// add_filter('cron_schedules', 'i8_register_daily_cron_schedule');
+// function i8_register_daily_cron_schedule($schedules)
+// {
+//     $schedules['i8_daily_cron'] = array(
+//         'interval' => (60 * 60) * 24,
+//         'display' => __('این کرون هر ۲۴ ساعت اجرا میشود')
+//     );
+//     $schedules['5minutes'] = array(
+//         'interval' => (5 * 60),
+//         'display' => __('این کرون هر ۵دقیقه اجرا میشود')
+//     );
+//     return $schedules;
+// }
+// add_action('remove_all_feed_on_feeds_table', 'remove_all_feed_on_feeds_table');
 
-function remove_all_feed_on_feeds_table()
-{
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'custom_rss_items';
-    $delete_status = $wpdb->query("DELETE FROM $table_name");
-    if ($delete_status) {
-        wp_safe_redirect(add_query_arg('success', 'true', wp_get_referer()));
-        exit;
-    } else {
-        echo '<div class="notice notice-error is-dismissible">
-                <p>مشکلی پیش آمد!</p>
-            </div>';
-    }
-}
-
-
-// Function to check if custom table exists and create it if not
-function custom_rss_parser_create_table()
-{
-
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'custom_rss_items';
-
-    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-        $charset_collate = $wpdb->get_charset_collate();
-
-        $sql = "CREATE TABLE $table_name (
-            id mediumint(9) NOT NULL AUTO_INCREMENT,
-            title text NOT NULL,
-            resource_name text NOT NULL,
-            resource_id mediumint(9) NOT NULL,
-            pub_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-            guid text NOT NULL,
-            PRIMARY KEY (id)
-        ) $charset_collate;";
-
-        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-        dbDelta($sql);
-    }
-}
-
-// Hook to handle the scheduled event
-add_action('custom_rss_parser_event', 'custom_rss_parser_run');
-
-// Function to parse and store RSS feed data
-function custom_rss_parser_run()
-{
-    // Replace 'YOUR_RSS_FEED_URL' with the actual RSS feed URL
-    $args = array(
-        'post_type' => 'resource',
-        'post_status' => 'publish',
-    );
-
-    $feeds_list = new WP_Query($args);
-
-    if ($feeds_list->have_posts()):
-        while ($feeds_list->have_posts()):
-            $feeds_list->the_post();
-            $rss_feed_url = get_post_meta(get_the_ID(), 'source_feed_link', true);
-            $source_root_link = get_post_meta(get_the_ID(), 'source_root_link', true);
-            $resource_id = get_the_ID();
-            $resource_name = get_the_title();
-            $need_to_merge_guid_link = get_post_meta(get_the_ID(), 'need_to_merge_guid_link', true);
-
-            // Fetch the RSS feed
-            $rss_feed = fetch_rss_feed($rss_feed_url);
-
-            if (!$rss_feed) {
-                return;
-            }
-
-            // Parse and store RSS feed data
-            foreach ($rss_feed->channel->item as $item) {
-                $title = $item->title;
-                $pub_date = date('Y-m-d H:i:s', strtotime($item->pubDate));
-
-                if ($need_to_merge_guid_link == 1) {
-                    $guid = $source_root_link . $item->guid . '';
-                } else {
-                    $guid = $item->guid . '';
-
-                }
-
-                // Check if the item already exists in the database
-                if (!custom_rss_parser_item_exists($guid)) {
-                    // Insert the new item into the custom table
-                    custom_rss_parser_insert_item($title, $pub_date, $guid, $resource_id, $resource_name);
-                }
+// function remove_all_feed_on_feeds_table()
+// {
+//     global $wpdb;
+//     $table_name = $wpdb->prefix . 'custom_rss_items';
+//     $delete_status = $wpdb->query("DELETE FROM $table_name");
+//     if ($delete_status) {
+//         wp_safe_redirect(add_query_arg('success', 'true', wp_get_referer()));
+//         exit;
+//     } else {
+//         echo '<div class="notice notice-error is-dismissible">
+//                 <p>مشکلی پیش آمد!</p>
+//             </div>';
+//     }
+// }
 
 
-            }
+// // Function to check if custom table exists and create it if not
+// function custom_rss_parser_create_table()
+// {
 
-        endwhile;
+//     global $wpdb;
+//     $table_name = $wpdb->prefix . 'custom_rss_items';
 
-    endif;
-}
+//     if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+//         $charset_collate = $wpdb->get_charset_collate();
 
-// Function to fetch the RSS feed
-function fetch_rss_feed($url)
-{
-    $response = wp_remote_get($url);
+//         $sql = "CREATE TABLE $table_name (
+//             id mediumint(9) NOT NULL AUTO_INCREMENT,
+//             title text NOT NULL,
+//             resource_name text NOT NULL,
+//             resource_id mediumint(9) NOT NULL,
+//             pub_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+//             guid text NOT NULL,
+//             PRIMARY KEY (id)
+//         ) $charset_collate;";
 
-    if (is_wp_error($response)) {
-        return false;
-    }
+//         require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+//         dbDelta($sql);
+//     }
+// }
 
-    $body = wp_remote_retrieve_body($response);
-    $rss_feed = simplexml_load_string($body);
-    return $rss_feed;
-}
+// // Hook to handle the scheduled event
+// add_action('custom_rss_parser_event', 'custom_rss_parser_run');
 
-// Function to check if an item already exists in the database
-function custom_rss_parser_item_exists($guid)
-{
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'custom_rss_items';
+// // Function to parse and store RSS feed data
+// function custom_rss_parser_run()
+// {
+//     // Replace 'YOUR_RSS_FEED_URL' with the actual RSS feed URL
+//     $args = array(
+//         'post_type' => 'resource',
+//         'post_status' => 'publish',
+//     );
 
-    $result = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE guid = %s", $guid . ''));
+//     $feeds_list = new WP_Query($args);
 
-    return $result > 0;
-}
+//     if ($feeds_list->have_posts()):
+//         while ($feeds_list->have_posts()):
+//             $feeds_list->the_post();
+//             $rss_feed_url = get_post_meta(get_the_ID(), 'source_feed_link', true);
+//             $source_root_link = get_post_meta(get_the_ID(), 'source_root_link', true);
+//             $resource_id = get_the_ID();
+//             $resource_name = get_the_title();
+//             $need_to_merge_guid_link = get_post_meta(get_the_ID(), 'need_to_merge_guid_link', true);
 
-// Function to insert a new item into the custom table
-function custom_rss_parser_insert_item($title, $pub_date, $guid, $resource_id, $resource_name)
-{
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'custom_rss_items';
+//             // Fetch the RSS feed
+//             $rss_feed = fetch_rss_feed($rss_feed_url);
 
-    $wpdb->insert(
-        $table_name,
-        array(
-            'title' => '' . $title,
-            'resource_name' => $resource_name,
-            'resource_id' => $resource_id,
-            'pub_date' => $pub_date,
-            'guid' => '' . $guid,
-        )
-    );
+//             if (!$rss_feed) {
+//                 return;
+//             }
 
-}
+//             // Parse and store RSS feed data
+//             foreach ($rss_feed->channel->item as $item) {
+//                 $title = $item->title;
+//                 $pub_date = date('Y-m-d H:i:s', strtotime($item->pubDate));
 
-require_once plugin_dir_path(__FILE__) . 'simple_html_dom.php';
-require_once plugin_dir_path(__FILE__) . 'resources_post_type.php';
+//                 if ($need_to_merge_guid_link == 1) {
+//                     $guid = $source_root_link . $item->guid . '';
+//                 } else {
+//                     $guid = $item->guid . '';
 
-include_once(plugin_dir_path(__FILE__) . 'menu.php');
-include_once(plugin_dir_path(__FILE__) . 'scraper.php');
-//Include jalali-date external library 
-require_once(plugin_dir_path(__FILE__) . 'jdatetime.class.php');
-// error_log(plugin_dir_path(__FILE__). 'admin/menu.php');
+//                 }
+
+//                 // Check if the item already exists in the database
+//                 if (!custom_rss_parser_item_exists($guid)) {
+//                     // Insert the new item into the custom table
+//                     custom_rss_parser_insert_item($title, $pub_date, $guid, $resource_id, $resource_name);
+//                 }
+
+
+//             }
+
+//         endwhile;
+
+//     endif;
+// }
+
+// // Function to fetch the RSS feed
+// function fetch_rss_feed($url)
+// {
+//     $response = wp_remote_get($url);
+
+//     if (is_wp_error($response)) {
+//         return false;
+//     }
+
+//     $body = wp_remote_retrieve_body($response);
+//     $rss_feed = simplexml_load_string($body);
+//     return $rss_feed;
+// }
+
+// // Function to check if an item already exists in the database
+// function custom_rss_parser_item_exists($guid)
+// {
+//     global $wpdb;
+//     $table_name = $wpdb->prefix . 'custom_rss_items';
+
+//     $result = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE guid = %s", $guid . ''));
+
+//     return $result > 0;
+// }
+
+// // Function to insert a new item into the custom table
+// function custom_rss_parser_insert_item($title, $pub_date, $guid, $resource_id, $resource_name)
+// {
+//     global $wpdb;
+//     $table_name = $wpdb->prefix . 'custom_rss_items';
+
+//     $wpdb->insert(
+//         $table_name,
+//         array(
+//             'title' => '' . $title,
+//             'resource_name' => $resource_name,
+//             'resource_id' => $resource_id,
+//             'pub_date' => $pub_date,
+//             'guid' => '' . $guid,
+//         )
+//     );
+
+// }
+
+// require_once plugin_dir_path(__FILE__) . 'simple_html_dom.php';
+// require_once plugin_dir_path(__FILE__) . 'resources_post_type.php';
+
+// include_once(plugin_dir_path(__FILE__) . 'menu.php');
+// include_once(plugin_dir_path(__FILE__) . 'scraper.php');
+// //Include jalali-date external library 
+// require_once(plugin_dir_path(__FILE__) . 'jdatetime.class.php');
+// // error_log(plugin_dir_path(__FILE__). 'admin/menu.php');
