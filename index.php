34b07181cff27dc0e0bd52a47f25a0d449db434d
Ali Mousavi <38689441+alimousavi68@users.noreply.github.com>
2024-08-07 10:34:53 +0330

    de active jdate

--- index.php
+++ index.php
@@ -23,8 +23,9 @@ function i8_pc_plugin_deactivate_self()
 // چک می‌کنیم آیا کلاس قبلاً تعریف شده است
 if (!class_exists('jDateTime')) {
     // Include jalali-date external library
-    require_once plugin_dir_path(__FILE__) . 'jdatetime.class.php';
+   // require_once plugin_dir_path(__FILE__) . 'jdatetime.class.php';
 }
+
 require_once ABSPATH . 'wp-admin/includes/file.php';
 require_once plugin_dir_path(__FILE__) . 'simple_html_dom.php';
 include_once (plugin_dir_path(__FILE__) . 'menu.php');
